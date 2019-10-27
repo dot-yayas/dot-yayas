16d01d76f04d5f2419796699e3850710b4b9cd6a
José Antonio Riaza Valverde <riazavalverde@gmail.com>
2019-10-27 15:59:04 +0100

    minor refactoring

--- src/main/scala/dot/yayas/types/YayasChar.scala
+++ src/main/scala/dot/yayas/types/YayasChar.scala
@@ -6,7 +6,7 @@ case class YayasChar(val value: Char) extends YayasType {
 	// Returns the dot-yayas type of the data (Character)
 	def get_yayas_type(): String = "Character"
 
-    // Returns a string representation of the dot-yayas character
+	// Returns a string representation of the dot-yayas character
 	override def to_string(): String = "'" + this.value.toString() + "'"
 
 }
--- src/main/scala/dot/yayas/types/YayasList.scala
+++ src/main/scala/dot/yayas/types/YayasList.scala
@@ -9,9 +9,9 @@ case class YayasList(val value: List[YayasType]) extends YayasType {
 	// Returns the dot-yayas type of the data (List)
 	def get_yayas_type(): String = "List"
 
-    // Returns a string representation of the dot-yayas list
+	// Returns a string representation of the dot-yayas list
 	override def to_string(): String =
-        "(" + this.value.map(x => x.to_string()).mkString(" ") + ")"
+		"(" + this.value.map(x => x.to_string()).mkString(" ") + ")"
 
 	// Applies a substitution to a dot-yayas list
 	override def apply(substitution: Map[YayasAtom, YayasType]): YayasType =
