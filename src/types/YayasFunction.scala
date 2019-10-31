2f290f3483693ae07589dfa7253c0217a22d287c
José Antonio Riaza Valverde <riazavalverde@gmail.com>
2019-10-31 02:40:30 +0100

    added function yayas type

---
+++ src/types/YayasFunction.scala
@@ -0,0 +1,15 @@
+package dot.yayas.types
+
+// Trait for dot-yayas functions
+case class YayasFunction(val value: (List[YayasAtom], YayasType)) extends YayasType {
+
+    // Returns the formal parameters of the yayas function
+    val formal_parameters: List[YayasAtom] = this.value._1
+
+    // Returns the body of the yayas function
+    val body: YayasType = this.value._2
+
+	// Returns the dot-yayas type of the data (Function)
+	def get_yayas_type(): String = "Function"
+
+}
