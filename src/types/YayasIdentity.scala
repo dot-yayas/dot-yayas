4786fdac1911067b80a9aaabee622c18a765cab6
José Antonio Riaza Valverde <riazavalverde@gmail.com>
2019-10-31 05:29:22 +0100

    added identity yayas type

---
+++ src/types/YayasIdentity.scala
@@ -0,0 +1,12 @@
+package dot.yayas.types
+
+// Class for dot-yayas identities
+case class YayasIdentity(val value: YayasType) extends YayasType {
+
+	// Returns the dot-yayas type of the data (Function)
+	def get_yayas_type(): String = "Identity"
+
+    // Returns a string representation of the dot-yayas data
+	override def show(): String = this.value.show()
+
+}
